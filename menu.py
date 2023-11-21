99ba16c67ce0f0703b2a3be7c8c5798c1c0c528b
WillBeazley <williambeazley2001@gmail.com>
2023-11-21 17:07:54 +0000

    Add option to download playlist

--- menu.py
+++ menu.py
@@ -1,19 +1,64 @@
-from spotifyExtractor import search_for_artist, get_songs_by_artist, get_token
+from spotifyExtractor import search_for_artist, get_songs_by_artist, get_token, search_for_playlist, get_playlist
 from googleExtractor import youtube_search
 from pprint import pprint
 import inquirer
 from pytube import YouTube
+import time
 
 token = get_token()
 
-while True:
+def playlists(search):
 
-    print("\n--Type exit at any time to leave program--\n")
+    result = search_for_playlist(token, search)
 
-    artist = input("Input an artist to find their top 10 songs: ")
+    playlists = []
+
+    for playlist in result:
+        playlists.append(playlist["name"])
+
+    choices = []
+    playlistDetails = {}
+
+    for playlist in result:
+        choices.append(playlist['name'])
+        playlistDetails[playlist["name"]] = playlist["id"]
+
+    choices.append("none")
+
+    questions = [
+        inquirer.List(
+            "playlist",
+            message="Here are the playlists, please select one for further details",
+            choices=choices,
+        ),
+    ]
+
+    playlist_choice = inquirer.prompt(questions)
+
+    if playlist_choice["playlist"] == "none":
+        return None
+
+    tracks = get_playlist(token, playlistDetails[playlist_choice["playlist"]])
+
+    for i, track in enumerate(tracks):
+        artist_name = track["track"]["artists"][0]["name"]
+        song_name = track["track"]["name"]
+
+        url = youtube_search(f"{song_name}, {artist_name}")
+
+        print(f"Downloading song {i+1}: {song_name} by {artist_name}")
+
+        YouTube(url).streams.filter(progressive=True).get_highest_resolution().download(f"Downloads/{playlist_choice['playlist']}")
+
+        time.sleep(3)
+
+
+    return playlist_choice
+
+def artists(artist):
 
     if artist.lower() == "exit":
-        break
+        return None
 
     find_artist = search_for_artist(token, artist)
     find_songs = get_songs_by_artist(token, find_artist["id"])
@@ -30,18 +75,54 @@ while True:
     questions = [
         inquirer.List(
             "song",
-            message="Here are their songs, please select one for further details:",
+            message="Here are their songs, please select one for further details",
             choices=choices,
         ),
     ]
 
     song_choice = inquirer.prompt(questions)
 
-    if song_choice['song'] == "none":
-        break
+    if song_choice["song"] == None:
+        return None
 
     url = youtube_search(f"{song_choice['song']}, {artist}")
 
     YouTube(url).streams.filter(progressive=True).get_highest_resolution().download("Downloads")
 
     print(f"{url} successfully downloaded")
+
+    return song_choice
+
+while True:
+
+    print("\n--Type exit at any time to leave program--\n")
+
+    type = [
+        inquirer.List(
+            "type",
+            message="Would you like to download a playlist or song?",
+            choices=["Playlist", "Song"],
+        )
+    ]
+
+    decision = inquirer.prompt(type)
+
+    if decision["type"] == "Song":
+
+        artist = input("Input an artist to find their top 10 songs: ")
+
+        song_choice = artists(artist)
+
+        if song_choice == None:
+            break
+
+    if decision["type"] == "Playlist":
+        
+        playlist = input("Input a playlist you would like to search for: ")
+
+        playlist_choice = playlists(playlist)
+
+        if playlist_choice == None:
+            break
+
+        
--- spotifyExtractor.py
+++ spotifyExtractor.py
@@ -17,6 +17,21 @@ def search_for_artist(token, artist_name):
     
     return json_result[0]
 
+def search_for_playlist(token, playlist_name):
+    url="https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={playlist_name}&type=playlist&limit=10"
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["playlists"]["items"]
+
+    if len(json_result) == 0:
+        print("No playlist found")
+        return None
+    
+    return json_result
+
 def get_songs_by_artist(token, artist_id):
     url=f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
     headers = get_auth_header(token)
@@ -24,8 +39,8 @@ def get_songs_by_artist(token, artist_id):
     json_result = json.loads(result.content)["tracks"]
     return json_result
 
-def get_users_playlists(token, user_id):
-    url=f"https://api.spotify.com/v1/users/{user_id}/playlists"
+def get_playlist(token, playlist_id):
+    url=f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
     headers = get_auth_header(token)
     result = get(url, headers=headers)
     json_result = json.loads(result.content)["items"]
